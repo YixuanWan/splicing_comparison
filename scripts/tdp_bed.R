6549dfacb8973f298222a3d638a2fbd0d5738fbd
YixuanWan <ewannwyx@gmail.com>
2023-02-08 17:51:01 +0000

    Scripts for tdp-43 BED files

---
+++ scripts/tdp_bed.R
@@ -0,0 +1,41 @@
+library(rtracklayer)
+library(GenomicRanges)
+library(dplyr)
+library(tidyr)
+library(data.table)
+library(plyranges)
+
+import_bed <- function(file){
+  df = data.table::as.data.table(import.bed(file))
+  return(df)
+}
+
+
+
+# import files
+suffix = '.bed'
+filepath = list.files("/Users/Ewann/splicing_comparison/data/tdp_bed",
+                   pattern = suffix,
+                   full.names = TRUE)
+
+tdp_bed = purrr::map(filepath, import_bed)
+
+# get sample id
+id = purrr::map(filepath, basename)
+id = gsub(suffix, "", id)
+
+# fill <name>with sample id
+tdp_bed = purrr::map2(tdp_bed, id, ~cbind(.x, name2 = .y))
+
+# combine them into a long 
+metatable = data.table::rbindlist(tdp_bed)
+
+meta_bed = metatable |> 
+  select(-name) |> 
+  mutate(name = name2, .keep = "unused") 
+
+# write out the file
+rtracklayer::export(meta_bed, "/Users/Ewann/splicing_comparison/data/tdp_bed/TARDBP_all.bed")
+
+
+
