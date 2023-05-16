53b42860e5b35a8420e3cec99e9dcb9d7a98dc4f
YixuanWan <ewannwyx@gmail.com>
2023-05-16 17:32:27 +0100

    Update tdp_sfsr3.R
    1. identify selective psi in SRSF3KD
    2. added scripts to illustrate the correlation among counts, psi, single psi.

--- scripts/tdp_sfsr3.R
+++ scripts/tdp_sfsr3.R
@@ -39,8 +39,7 @@ shared_cj |>
   scale_fill_brewer(palette = "Set3") +
   labs(
     title = "Shared cryptic junctions in TDP43KD and SRSF3KD",
-    fill = "Junction type"
-  )
+    fill = "Junction type")
 
 # PSI comparison
 psi_cj = tdpkd |> 
@@ -125,6 +124,114 @@ tdpkd |>
   filter(control_mean_psi < 0.05 & tdp43kd_mean_psi > 0.10) |> 
   arrange(-tdp43kd_mean_psi) |> filter(gene_name == "HDGFL2")
 
+# check overlap between nygc-tdp-cortex dataset and srsf3kd
+nygc_cj = nygc |>
+  filter(tdp_path == "path") |>
+  filter(disease_tissue == TRUE) |> 
+  filter(grepl("Cortex", tissue_clean)) |> 
+  pull(paste_into_igv_junction) |> 
+  unique()
+
+nygc_srsf3kd = srsf3kd |> 
+  dplyr::select(gene_name,paste_into_igv_junction,junc_cat,probability_changing,control_mean_psi,srsf3kd_mean_psi,strand) |>  
+  filter(control_mean_psi < 0.05 & srsf3kd_mean_psi > 0.10) |> 
+  arrange(-srsf3kd_mean_psi) |> 
+  filter(paste_into_igv_junction %in% nygc_cj) |> 
+  filter(junc_cat != "annotated") |> 
+  distinct(paste_into_igv_junction, .keep_all = TRUE) |> 
+  filter(junc_cat != "none")
+
+counts = fread("~/cluster/first_weeks/splicing_comparisons/tdp_srsf3/sj_tabs_parsed/srsf3kd_counts.aggregated.clean.annotated.bed")
+srsf3_counts = counts |> 
+  mutate(seq = paste0(V1, ":", V2, "-", V3), .keep= "unused") |> 
+  filter(grepl("^SRSF3KD", V4)) |> 
+  filter(!grepl("annotated", V7)) |> 
+  filter(!grepl("none", V7)) |> 
+  filter(seq %in% nygc_cj) 
+
+mcol_srsf3 = data.frame(stringr::str_split_fixed(srsf3_counts$V7, "[|]", 3)) 
+names(mcol_srsf3) = c("gene", "junc_cat", "n_tdpkd_dataset")
+srsf3_counts = cbind(srsf3_counts, mcol_srsf3)
+
+srsf3_counts = srsf3_counts |> 
+  select(-V7) |> 
+  group_by(seq) |> 
+  mutate(mean_counts = mean(V5)) |> 
+  ungroup() |> 
+  distinct(seq, .keep_all = TRUE) |> 
+  mutate(pass_psi = ifelse((seq %in% nygc_srsf3kd$paste_into_igv_junction), TRUE, FALSE))
+
+srsf3kd_psi = srsf3kd |> 
+  select(paste_into_igv_junction, srsf3kd_mean_psi, control_mean_psi)
+
+srsf3_counts_psi = srsf3_counts |> 
+  left_join(srsf3kd_psi, by = c("seq" = "paste_into_igv_junction")) |> 
+  filter(!is.na(srsf3kd_mean_psi)) |> 
+  mutate(delta_psi = log2(srsf3kd_mean_psi/control_mean_psi)) |> 
+  select(-V4, -V5, -V6) |> 
+  unique()
+  
+
+srsf3_counts_psi |> 
+  ggplot(aes(x = delta_psi, y = mean_counts)) +
+  geom_point(aes(colour = junc_cat, shape = pass_psi)) +
+  theme_minimal() +
+  ggrepel::geom_text_repel(aes(label = gene), size = 3, max.overlaps = 10) +
+  labs(
+    x = "Mean PSI_Log2(SRSF3KD/Control)",
+    y = "Counts",
+    colour = "Pass PSI test?"
+  ) +
+  # theme(
+  #   axis.text.x = element_blank()
+  # ) +
+  # scale_x_continuous(trans = scales::pseudo_log_trans(sigma = 0.5)) +
+  scale_y_continuous(trans = scales::pseudo_log_trans(sigma = 0.5)) 
+  # scale_colour_manual(values = c('black', 'red')) 
+  
+
+# Are counts correlated with mean_psi?
+srsf3_sg1 = fread("~/cluster/first_weeks/splicing_comparisons/tdp_srsf3/majiq/psi_voila_tsv_single/SRSF3KD_rep1.Aligned.sorted.out_parsed.csv")
+srsf3_sg2 = fread("~/cluster/first_weeks/splicing_comparisons/tdp_srsf3/majiq/psi_voila_tsv_single/SRSF3KD_rep2.Aligned.sorted.out_parsed.csv")
+
+srsf3_sg1_psi = srsf3_sg1 |> 
+  select(gene_name, mean_psi_per_lsv_junction, paste_into_igv_junction) |> 
+  filter(paste_into_igv_junction %in% srsf3_counts_psi$seq) |> 
+  mutate(mean_psi_rep1 = mean_psi_per_lsv_junction, .keep = "unused")
+
+srsf3_sg2_psi = srsf3_sg2 |> 
+  select(gene_name, mean_psi_per_lsv_junction, paste_into_igv_junction) |> 
+  filter(paste_into_igv_junction %in% srsf3_counts_psi$seq) |> 
+  mutate(mean_psi_rep2 = mean_psi_per_lsv_junction, .keep = "unused")
+
+srsf3_sg_psi = srsf3_counts |> 
+  left_join(srsf3_sg1_psi, by = c("seq" = "paste_into_igv_junction", "gene" ="gene_name"), relationship = "many-to-many") |> 
+  left_join(srsf3_sg2_psi, by = c("seq" = "paste_into_igv_junction", "gene" ="gene_name"), relationship = "many-to-many") |> 
+  mutate(mean_single_psi = (mean_psi_rep1 + mean_psi_rep2)/2) |> 
+  select(-V4, -V5, -V6)  |> 
+  group_by(seq, junc_cat) |> 
+  mutate(mean_sg_psi = mean(mean_single_psi), .keep = "unused") |> 
+  ungroup() |> 
+  distinct(seq, junc_cat, .keep_all = TRUE) |> 
+  replace(is.na(srsf3_sg_psi), 0)
+
+srsf3_sg_psi |> 
+  ggplot(aes(y = mean_counts, x = mean_sg_psi, colour = pass_psi)) +
+  geom_point(aes(shape = ifelse((mean_sg_psi > 0), "Yes", "No"))) +
+  theme_minimal() +
+  ggrepel::geom_text_repel(aes(label = gene), size = 3, max.overlaps = 5) +
+  scale_colour_manual(values = c('black', 'red')) +
+  scale_fill_manual(values = c('black', 'red')) +
+  scale_x_continuous(trans = scales::pseudo_log_trans(sigma = 0.5)) +
+  scale_y_continuous(trans = scales::pseudo_log_trans(sigma = 0.5)) +
+  labs(
+    y = "Counts",
+    x = "Mean single PSI",
+    colour = "Pass PSI test?",
+    shape = "Have single PSI?"
+  ) + 
+  scale_shape_manual(values = c(4, 16))
+
 
 # plot numbers of cryptic junctions in TDP43KD, SRSF3KD, and double KD groups
 tdpkd_cj = tdpkd |> 
@@ -155,9 +262,13 @@ tdpsrsf3kd_cj = tdpsrsf3kd |>
   mutate(doubleKD = TRUE) |> 
   distinct(paste_into_igv_junction, junc_cat, .keep_all = TRUE)
 
+nygc_srsf3kd_cj = nygc_srsf3kd |> 
+  select(paste_into_igv_junction, junc_cat) |> 
+  mutate(nygc_srsf3kd = TRUE)
  
 overlap_cj = full_join(tdpkd_cj, srsf3kd_cj, by = c("paste_into_igv_junction", "junc_cat"), relationship = "many-to-many") |> 
   full_join(tdpsrsf3kd_cj, by = c("paste_into_igv_junction", "junc_cat"), relationship = "many-to-many") |> 
+  full_join(nygc_srsf3kd_cj, by = c("paste_into_igv_junction", "junc_cat"), relationship = "many-to-many") |> 
   mutate(gene_id = ifelse(is.na(gene_name.x), gene_name.y, gene_name.x), .keep = "unused") |> 
   mutate(gene_id = ifelse(is.na(gene_id), gene_name, gene_id), .keep = "unused") |> 
   filter(junc_cat != 'none') |> 
@@ -165,9 +276,10 @@ overlap_cj = full_join(tdpkd_cj, srsf3kd_cj, by = c("paste_into_igv_junction", "
 
 
 overlap_cj |> 
-  select(tdpKD, srsf3KD, doubleKD) |> 
+  select(tdpKD, srsf3KD, doubleKD, nygc_srsf3kd) |> 
   eulerr::euler() |> 
-  plot(quantities = TRUE, main = "Number of cyptic junctions", legend = list(labels = c("TDP43KD", "SRSF3KD", "DoubleKD")))
+  plot(quantities = TRUE, main = "Number of cyptic junctions", 
+       legend = list(labels = c("TDP43KD", "SRSF3KD", "DoubleKD", "NYGC_SRSF3KD")))
 
 cj_longer = overlap_cj |> 
   pivot_longer(cols = ends_with("KD"),
